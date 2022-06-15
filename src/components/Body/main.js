f4341e613fe8b1975a2b1bbfe03c3466cce6a661
Ramiro Aragon Paz <ramiroaragonpaz@gmail.com>
2022-06-15 18:31:47 -0300

    images feature

--- src/components/Body/main.js
+++ src/components/Body/main.js
@@ -1,22 +1,28 @@
+import { useState } from 'react'
 
-import { dividerClasses } from '@mui/material';
-import { useState } from 'react';
+import "./main.css"
+
 
 function Body(){
 const [poke, setPoke] = useState([])
 const [skill, setSkill] = useState([])
 const [first, setFirst] = useState(true)
-
+const [img, setImg] = useState('')
 
 const getPokemon = ()=>{
     setFirst(false)
-    let random = Math.floor(Math.random()*247);
+    document.getElementById('img').className = "imgPoke"
+    document.getElementById('imgDiv').className = ''
+    document.getElementById('h2Name').className = 'name'
+    let random = Math.floor(Math.random()*247)
     fetch("https://pokeapi.co/api/v2/pokemon/" + random)
     .then(response => {
         return response.json()
     })
     .then((pokemon) => {
-        setPoke((pokemon.name).toUpperCase())        
+        console.log(pokemon)
+        setPoke((pokemon.name).toUpperCase())
+        console.log(pokemon.name)
     })
     .catch(err => {
         console.log(err);
@@ -26,21 +32,60 @@ const getPokemon = ()=>{
         return response.json()
     })
     .then((skill) => {
-        setSkill(skill.effect_entries[1].short_effect)        
+        console.log(skill)
+        if (skill.effect_entries.length == 2){
+            setSkill(skill.effect_entries[1].short_effect)
+        } else {
+            setSkill(skill.effect_entries[0].short_effect)
+        }        
     })
     .catch(err => {
         console.log(err);
     })
+    let imgPoke = "https://unpkg.com/pokeapi-sprites@2.0.4/sprites/pokemon/other/dream-world/" + random + ".svg"
+    fetch(imgPoke)
+    .then(response => {
+        return response
+    })
+    .then((imgenPokemon) => {
+        console.log(imgenPokemon.url)
+        setImg(imgenPokemon.url)        
+        console.log(img)
+    })
+}
+function revealPokemon() {
+    let nameInputed = document.getElementById('nameInput').value.toUpperCase()
+    console.log(nameInputed)
+    let nameCorrect = poke
+    console.log(nameCorrect)
+    if (nameCorrect == nameInputed ){
+        document.getElementById('img').className = "imgPokeDiscover"
+        document.getElementById('imgDiv').className = 'name'
+        document.getElementById('h2Name').className = 'reveal'
+    }
+}
+function revealName() {
+    document.getElementById('img').className = "imgPokeDiscover"
+    document.getElementById('imgDiv').className = 'name'
+    document.getElementById('h2Name').className = 'reveal'
 }
     return(
         <div>
-            {first ? <><h1>Discover a new Pokemon</h1><button onClick={getPokemon}>START!</button></>
+            {first ? <><h1>Discover a new Pokemon</h1><button  variant="contained" onClick={getPokemon}>START!</button></>
             : (
                 <>
                 <h1>Do you know this Pokemon?</h1>
-                <h2>Name: {poke}</h2>
+                <div id="imagen">
+                    <img src={`${img}` } className="imgPoke" id="img"/>
+                </div>
+                <div id='imgDiv'>
+                    <input name='name' id='nameInput' label="Name" variant="outlined" />
+                    <button  type='button' onClick={revealPokemon} variant="contained" color="error" >Try!</button>
+                    <button  type='button' onClick={revealName} variant="contained" className="btn" >Reveal!</button>
+                </div>
+                <h2 id="h2Name"className='name'>Name: {poke}</h2>
                 <h3>Abilities:  {skill}</h3>
-                <button onClick={getPokemon}>Next</button>
+                <button   variant="contained" onClick={getPokemon} className="btn" >Next</ button>
                 </>
             )
             }
